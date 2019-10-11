5f36cbd1bde3fec0ab05657be9444ae2f4fff153
xiapeimin <33412233+xiapeimin@users.noreply.github.com>
2019-10-11 19:13:20 +0800

    Update xpmfmtwxmsg.js

--- xpmfmtwxmsg.js
+++ xpmfmtwxmsg.js
@@ -54,7 +54,7 @@ module.exports = function formatMsg (data) {
                     <ToUserName><![CDATA[${data.touser}]]></ToUserName>
                     <FromUserName><![CDATA[${data.fromuser}]]></FromUserName>
                     <MsgType><![CDATA[text]]></MsgType>
-                    <Content><![CDATA[该类行不被支持]]></Content>
+                    <Content><![CDATA[该类型不被支持！]]></Content>
                     <CreateTime>${data.msgtime}</CreateTime>
                 </xml>
             `;
